c8f9dd6053c4bf220b17b6ee8b8b34c96414deb3
Connor Delacruz <connordelacruz.cd@gmail.com>
2025-02-21 23:44:14 -0800

    calendar visibility

--- Calendar.java
+++ Calendar.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeParseException;
 import java.util.Set;
 import java.util.TimeZone;
 import java.util.ArrayList;
@@ -87,19 +88,27 @@ public class Calendar {
                     System.out.println("Returning to main menu.");
                     return;
                 case "a":
-                    System.out.print("Enter event name: ");
-                    String eventName = reader.readLine().trim();
-                    System.out.print("Enter event start time (YYYY-MM-DDTHH:MM): ");
-                    LocalDateTime startTime = LocalDateTime.parse(reader.readLine().trim());
-                    System.out.print("Enter event end time (YYYY-MM-DDTHH:MM): ");
-                    LocalDateTime endTime = LocalDateTime.parse(reader.readLine().trim());
-                    System.out.print("Enter event description: ");
-                    String description = reader.readLine().trim();
-                    addEvent(new Event(eventName, startTime, endTime, description));
-                    System.out.println("\nEvent added.");
+                    try {
+                        System.out.print("Enter event name: ");
+                        String eventName = reader.readLine().trim();
+                        System.out.print("Enter event start time (YYYY-MM-DDTHH:MM): ");
+                        LocalDateTime startTime = LocalDateTime.parse(reader.readLine().trim());
+                        System.out.print("Enter event end time (YYYY-MM-DDTHH:MM): ");
+                        LocalDateTime endTime = LocalDateTime.parse(reader.readLine().trim());
+                        System.out.print("Enter event description: ");
+                        String description = reader.readLine().trim();
+                        addEvent(new Event(eventName, startTime, endTime, description));
+                        System.out.println("\nEvent added.");
+                    } catch (DateTimeParseException e) {
+                        System.out.println("Error parsing date-time input (Make sure you use the format YYYY-MM-DDTHH:MM with the T in-between DD and HH.");
+                    }
                     break;
                 case "e":
-
+                    break;
+                case "t":
+                    System.out.println("Changed calendar visibility to: " + !this.isPrivate);
+                    this.isPrivate = !this.isPrivate;
+                    break;
             }
         }
     }
